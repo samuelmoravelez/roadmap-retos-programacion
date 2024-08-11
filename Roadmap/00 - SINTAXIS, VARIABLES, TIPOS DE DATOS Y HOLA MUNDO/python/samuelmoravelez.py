a9199388e61dc4b40d1dc6e8dc4e5529d9ce8604
samuelmoravelez <samuelmoravelez52@gmail.com>
2024-08-11 13:47:20 -0500

    Create samuelmoravelez.py
    .py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/samuelmoravelez.py
@@ -0,0 +1,26 @@
+# https://python.org
+# comentario en una linea
+
+"""
+este es comentario en varias lineas
+"""
+
+'''
+este tambien es un comentario en varias 
+lineas
+'''
+
+my_variable = "mi variable"
+my_variable = "nuevo valor a variable"
+
+MY_CONSTAN = "esto es una constante "
+
+
+my_int  = 2
+my_float = 0.5
+my_bool = true
+my_bool = false
+my_str = "cadena de texto"
+my_other_str = 'mi otra cadena de texto'
+
+print ("hola, phyton")
